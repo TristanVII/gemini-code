f7d2e6710cbab68171e084cfb376fef0abe74f85
TristanVII <tristandavis888@gmail.com>
2025-05-06 23:21:41 -0700

    progress. Create SQLiteDB

--- geminicode/work_tree/tree.py
+++ geminicode/work_tree/tree.py
@@ -1,6 +1,16 @@
 import os
-
+import sqlite3
+import time
 from geminicode.utils.files import get_git_ignore_file_content, read_file
+DB_SCHEMA = """
+    CREATE TABLE IF NOT EXISTS project_files (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        path TEXT UNIQUE NOT NULL,
+        last_modified REAL NOT NULL,
+        content TEXT NOT NULL,
+    );
+    CREATE INDEX IF NOT EXISTS idx_path ON project_files (path);
+    """
 class WorkTree:
     def __init__(self, ctx):
         self.ctx = ctx
@@ -15,13 +25,20 @@ class WorkTree:
     def set_project_index_file_path(self):
         base_dir = '/tmp'
         project_name = os.path.basename(self.ctx.cwd)
-        self.project_index_file_path = os.path.join(base_dir, project_name, 'project_index.txt')
-        print('Project index file path:', self.project_index_file_path)
+        self.project_index_path = os.path.join(base_dir, project_name)
+        print('Project index file path:', self.project_index_path)
 
     def add_git_ignore_files(self):
         ignored_patterns = get_git_ignore_file_content(self.ctx.cwd)
         if ignored_patterns:
             self.ctx.ignored_files.extend(ignored_patterns)
+    
+    def _init_db(self):
+        os.makedirs(self.project_index_path, exist_ok=True)
+        self.conn = sqlite3.connect(self.project_index_path)
+        cursor = self.conn.cursor()
+        cursor.executescript(self.DB_SCHEMA)
+        self.conn.commit()
 
     def walk_files(self, start_dir, ignored_files=[]):
         for dirpath, _, filenames in os.walk(start_dir):
@@ -34,16 +51,12 @@ class WorkTree:
                     continue
                 yield os.path.join(dirpath, filename)
     
-    def write_project_index(self):
-        index_dir = os.path.dirname(self.project_index_file_path)
-        os.makedirs(index_dir, exist_ok=True)
-
+    def save_project_db(self):
         for file_path in self.walk_files(self.ctx.cwd, self.ctx.ignored_files):
-            content = read_file(file_path)
+            content = read_file(file_path).strip()
             if content:
-                formatted_entry = self.format_project_index_file(file_path, content)
-                with open(self.project_index_file_path, 'a', encoding='utf-8') as index_file:
-                    index_file.write(formatted_entry)
+                self.conn.execute("INSERT INTO project_files (path, content, last_modified) VALUES (?, ?, ?)", (file_path, content, time.time()))
 
+    # TODO: ADD LOOP DB TO CREATE THE CACHED FILE
     def format_project_index_file(self, file_path, content):
         return f"{self.seperator}{file_path}{self.seperator}\n{content}{self.seperator}\nEND{self.seperator}\n"
